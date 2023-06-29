47ed7fe429d6cf72fe3598329a9639a084b54739
Roger <rogerfutsa@gmail.com>
2023-06-29 18:20:13 -0300

    atualizado

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/eu.jpg")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/eu.jpg")
   }
 }
-
